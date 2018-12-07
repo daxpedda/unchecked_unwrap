0a1a20125d24c896061087ba8d2c6d671f6a40c5
daxpedda <daxpedda@users.noreply.github.com>
2018-12-07 13:46:36 +0100

    More fixes.

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,102 +1,109 @@
 #![feature(test)]
 
-extern crate test;
-use self::test::{black_box, Bencher};
-use unchecked_unwrap::*;
+mod checked {
+    extern crate test;
+    use self::test::{black_box, Bencher};
 
-#[bench]
-fn option_expect(b: &mut test::Bencher) {
-    let r = Some(0).as_ref();
+    #[bench]
+    fn option_expect(b: &mut Bencher) {
+        let r = Some(0).as_ref();
 
-    b.iter(|| {
-        for _ in 0..1000 {
-            black_box(black_box(r).expect(""));
-        }
-    });
-}
-
-#[bench]
-fn option_unchecked_expect(b: &mut Bencher) {
-    let r = Some(0).as_ref();
-
-    b.iter(|| {
-        for _ in 0..1000 {
-            unsafe {
-                black_box(black_box(r).unchecked_expect(""));
+        b.iter(|| {
+            for _ in 0..1000 {
+                black_box(black_box(r).expect(""));
             }
-        }
-    });
-}
+        });
+    }
 
-#[bench]
-fn result_expect(b: &mut test::Bencher) {
-    let r: Result<_, &()> = Ok(0).as_ref();
+    #[bench]
+    fn result_expect(b: &mut test::Bencher) {
+        let r: Result<_, &()> = Ok(0).as_ref();
 
-    b.iter(|| {
-        for _ in 0..1000 {
-            black_box(black_box(r).expect(""));
-        }
-    });
-}
-
-#[bench]
-fn result_unchecked_expect(b: &mut Bencher) {
-    let r: Result<_, &()> = Ok(0).as_ref();
-
-    b.iter(|| {
-        for _ in 0..1000 {
-            unsafe {
-                black_box(black_box(r).unchecked_expect(""));
+        b.iter(|| {
+            for _ in 0..1000 {
+                black_box(black_box(r).expect(""));
             }
-        }
-    });
-}
+        });
+    }
 
-#[bench]
-fn option_unwrap(b: &mut test::Bencher) {
-    let r = Some(0).as_ref();
+    #[bench]
+    fn option_unwrap(b: &mut test::Bencher) {
+        let r = Some(0).as_ref();
 
-    b.iter(|| {
-        for _ in 0..1000 {
-            black_box(black_box(r).unwrap());
-        }
-    });
-}
-
-#[bench]
-fn option_unchecked_unwrap(b: &mut Bencher) {
-    let r = Some(0).as_ref();
-
-    b.iter(|| {
-        for _ in 0..1000 {
-            unsafe {
-                black_box(black_box(r).unchecked_unwrap());
+        b.iter(|| {
+            for _ in 0..1000 {
+                black_box(black_box(r).unwrap());
             }
-        }
-    });
-}
+        });
+    }
 
-#[bench]
-fn result_unwrap(b: &mut test::Bencher) {
-    let r: Result<_, &()> = Ok(0).as_ref();
+    #[bench]
+    fn result_unwrap(b: &mut test::Bencher) {
+        let r: Result<_, &()> = Ok(0).as_ref();
 
-    b.iter(|| {
-        for _ in 0..1000 {
-            #[allow(clippy::option_unwrap_used)]
-            black_box(black_box(r).unwrap());
-        }
-    });
-}
-
-#[bench]
-fn result_unchecked_unwrap(b: &mut Bencher) {
-    let r: Result<_, &()> = Ok(0).as_ref();
-
-    b.iter(|| {
-        for _ in 0..1000 {
-            unsafe {
-                black_box(black_box(r).unchecked_unwrap());
+        b.iter(|| {
+            for _ in 0..1000 {
+                #[allow(clippy::option_unwrap_used)]
+                black_box(black_box(r).unwrap());
             }
-        }
-    });
+        });
+    }
+}
+
+mod unchecked {
+    extern crate test;
+    use self::test::{black_box, Bencher};
+    use unchecked_unwrap::*;
+
+    #[bench]
+    fn option_unchecked_expect(b: &mut Bencher) {
+        let r = Some(0).as_ref();
+
+        b.iter(|| {
+            for _ in 0..1000 {
+                unsafe {
+                    black_box(black_box(r).unchecked_expect(""));
+                }
+            }
+        });
+    }
+
+    #[bench]
+    fn result_unchecked_expect(b: &mut Bencher) {
+        let r: Result<_, &()> = Ok(0).as_ref();
+
+        b.iter(|| {
+            for _ in 0..1000 {
+                unsafe {
+                    black_box(black_box(r).unchecked_expect(""));
+                }
+            }
+        });
+    }
+
+    #[bench]
+    fn option_unchecked_unwrap(b: &mut Bencher) {
+        let r = Some(0).as_ref();
+
+        b.iter(|| {
+            for _ in 0..1000 {
+                unsafe {
+                    black_box(black_box(r).unchecked_unwrap());
+                }
+            }
+        });
+    }
+
+    #[bench]
+    fn result_unchecked_unwrap(b: &mut Bencher) {
+        let r: Result<_, &()> = Ok(0).as_ref();
+
+        b.iter(|| {
+            for _ in 0..1000 {
+                unsafe {
+                    black_box(black_box(r).unchecked_unwrap());
+                }
+            }
+        });
+    }
 }
