19cdec8102598353290952d50cba21c26450658a
Travis CI User <travis@example.org>
2018-12-08 00:16:06 +0000

    Automatic Travis documentation build

--- master/aliases.js
+++ master/aliases.js
@@ -1,3 +1,4 @@
 var ALIASES = {};
 ALIASES['unchecked_unwrap'] = {};
 ALIASES['unchecked_unwrap'] = {};
+ALIASES['unchecked_unwrap'] = {};
--- master/source-files.js
+++ master/source-files.js
@@ -1,3 +1,4 @@
 var N = null;var sourcesIndex = {};
 sourcesIndex['unchecked_unwrap'] = {"name":"","dirs":[],"files":["lib.rs"]};
 sourcesIndex['unchecked_unwrap'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['unchecked_unwrap'] = {"name":"","dirs":[],"files":["lib.rs"]};
