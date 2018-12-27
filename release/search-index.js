f6b507904bd947bbe7a3d8dc18745877e03ab412
Travis CI User <travis@example.org>
2018-12-27 13:49:58 +0000

    Automatic Travis documentation build

---
+++ release/search-index.js
@@ -0,0 +1,3 @@
+var N = null;var searchIndex = {};
+searchIndex["unchecked_unwrap"]={"doc":"unchecked_unwrap","items":[[8,"UncheckedExpect","unchecked_unwrap","Trait for `unchecked_expect`.",N,N],[10,"unchecked_expect","","Unwraps an [`Option`] or [`Result`], yielding the content…",0,[[["self"],["str"]],["t"]]],[8,"UncheckedUnwrap","","Trait for `unchecked_unwrap`.",N,N],[10,"unchecked_unwrap","","Unwraps an [`Option`] or [`Result`], yielding the content…",1,[[["self"]],["t"]]]],"paths":[[8,"UncheckedExpect"],[8,"UncheckedUnwrap"]]};
+initSearch(searchIndex);addSearchOptions(searchIndex);
---
+++ release/source-files.js
@@ -0,0 +1,2 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["unchecked_unwrap"] = {"name":"","dirs":[],"files":["lib.rs"]};
