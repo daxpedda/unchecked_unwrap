6db2467287609222242dba10a7ea1e3a91bf4a95
Travis CI User <travis@example.org>
2019-02-08 14:27:39 +0000

    Automatic Travis documentation build

--- release/search-index.js
+++ release/search-index.js
@@ -1,5 +1,5 @@
 var N=null,E="",T="t",U="u",searchIndex={};
-var r_aa="unchecked_unwrap",r_ba="Unwraps an [`Option`] or [`Result`], yielding the content…",r_ca="UncheckedExpect",r_da="UncheckedUnwrap";
+var R=["unchecked_unwrap","Unwraps an [`Option`] or [`Result`], yielding the content…","UncheckedExpect","UncheckedUnwrap"];
 
-searchIndex[r_aa]={"doc":r_aa,"i":[[8,r_ca,r_aa,"Trait for `unchecked_expect`.",N,N],[10,"unchecked_expect",E,r_ba,0,[[["self"],["str"]],[T]]],[8,r_da,E,"Trait for `unchecked_unwrap`.",N,N],[10,r_aa,E,r_ba,1,[[["self"]],[T]]]],"p":[[8,r_ca],[8,r_da]]};
+searchIndex[R[0]]={"doc":R[0],"i":[[8,R[2],R[0],"Trait for `unchecked_expect`.",N,N],[10,"unchecked_expect",E,R[1],0,[[["self"],["str"]],[T]]],[8,R[3],E,"Trait for `unchecked_unwrap`.",N,N],[10,R[0],E,R[1],1,[[["self"]],[T]]]],"p":[[8,R[2]],[8,R[3]]]};
 initSearch(searchIndex);addSearchOptions(searchIndex);
