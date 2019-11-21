8e5efe269f0719345172d8eaea14975718f4b35a
Travis CI User <travis@example.org>
2019-11-21 16:37:25 +0000

    Automatic Travis documentation build

--- release/doc/search-index.js
+++ release/doc/search-index.js
@@ -2,4 +2,4 @@ var N=null,E="",T="t",U="u",searchIndex={};
 var R=["unchecked_unwrap","Unwraps an [`Option`] or [`Result`], yielding the content…","UncheckedExpect","UncheckedUnwrap"];
 
 searchIndex["unchecked_unwrap"]={"doc":R[0],"i":[[8,R[2],R[0],"Trait for `unchecked_expect`.",N,N],[10,"unchecked_expect",E,R[1],0,[[["str"]],[T]]],[8,R[3],E,"Trait for `unchecked_unwrap`.",N,N],[10,R[0],E,R[1],1,[[],[T]]]],"p":[[8,R[2]],[8,R[3]]]};
-initSearch(searchIndex);addSearchOptions(searchIndex);
+addSearchOptions(searchIndex);initSearch(searchIndex);
