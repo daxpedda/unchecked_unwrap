2a909c4eeb7415a4cbeb3fd0e3d64344610a0ad5
daxpedda <daxpedda@gmail.com>
2020-05-08 11:33:42 +0000

    Deploying to gh-pages from  @ 5113e8774859fda15fd6aace78582379fc1291d2 🚀

--- master/doc/search-index.js
+++ master/doc/search-index.js
@@ -1,5 +1,4 @@
-var N=null,E="",T="t",U="u",searchIndex={};
-var R=["unchecked_unwrap","Unwraps an [`Option`] or [`Result`], yielding the content…","UncheckedExpect","UncheckedUnwrap"];
-
-searchIndex[R[0]]={"doc":R[0],"i":[[8,R[2],R[0],"Trait for `unchecked_expect`.",N,N],[10,"unchecked_expect",E,R[1],0,[[["self"],["str"]],[T]]],[8,R[3],E,"Trait for `unchecked_unwrap`.",N,N],[10,R[0],E,R[1],1,[[["self"]],[T]]]],"p":[[8,R[2]],[8,R[3]]]};
-initSearch(searchIndex);addSearchOptions(searchIndex);
+var searchIndex = JSON.parse('{\
+"unchecked_unwrap":{"doc":"unchecked_unwrap","i":[[8,"UncheckedExpect","unchecked_unwrap","Trait for `unchecked_expect`.",null,null],[10,"unchecked_expect","","Unwraps an [`Option`] or [`Result`], yielding the content…",0,[[]]],[8,"UncheckedUnwrap","","Trait for `unchecked_unwrap`.",null,null],[10,"unchecked_unwrap","","Unwraps an [`Option`] or [`Result`], yielding the content…",1,[[]]]],"p":[[8,"UncheckedExpect"],[8,"UncheckedUnwrap"]]}\
+}');
+addSearchOptions(searchIndex);initSearch(searchIndex);
