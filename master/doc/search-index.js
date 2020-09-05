fc82783fdf69db45f13289f019777aa168942961
daxpedda <daxpedda@users.noreply.github.com>
2020-09-05 00:23:03 +0000

    Deploying to gh-pages from  @ 0e54f1eea11224241cf685ba3b936c18ee89a996 🚀

--- master/doc/search-index.js
+++ master/doc/search-index.js
@@ -1,4 +1,4 @@
 var searchIndex = JSON.parse('{\
-"unchecked_unwrap":{"doc":"unchecked_unwrap","i":[[8,"UncheckedExpect","unchecked_unwrap","Trait for `unchecked_expect`.",null,null],[10,"unchecked_expect","","Unwraps an [`Option`] or [`Result`], yielding the content…",0,[[]]],[8,"UncheckedUnwrap","","Trait for `unchecked_unwrap`.",null,null],[10,"unchecked_unwrap","","Unwraps an [`Option`] or [`Result`], yielding the content…",1,[[]]]],"p":[[8,"UncheckedExpect"],[8,"UncheckedUnwrap"]]}\
+"unchecked_unwrap":{"doc":"unchecked_unwrapCrates.io Libraries.io Commits since…","i":[[8,"UncheckedExpect","unchecked_unwrap","Trait for `unchecked_expect`.",null,null],[10,"unchecked_expect","","Unwraps an [`Option`] or [`Result`], yielding the content…",0,[[]]],[8,"UncheckedUnwrap","","Trait for `unchecked_unwrap`.",null,null],[10,"unchecked_unwrap","","Unwraps an [`Option`] or [`Result`], yielding the content…",1,[[]]]],"p":[[8,"UncheckedExpect"],[8,"UncheckedUnwrap"]]}\
 }');
 addSearchOptions(searchIndex);initSearch(searchIndex);
